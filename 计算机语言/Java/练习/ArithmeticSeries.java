e8bed74aa455efedf4286b2fc5c12a5201759770
Xx <xuexing9527@163.com>
2025-11-27 00:25:42 +0800

    learn: java

--- 计算机语言/Java/练习/ArithmeticSeries.java
+++ 计算机语言/Java/练习/ArithmeticSeries.java
@@ -7,6 +7,7 @@ class ArithmeticSeries {
         //     a += a + i;
         // }
         // return a + b;
+        System.out.println(String[] args)
     }
     public int sum (int a, int b) {
         for(int i = 0; i < b - a; i++) {
@@ -15,3 +16,5 @@ class ArithmeticSeries {
         return a + b;
     }
 }
+
+const A = new ArithmeticSeries(1, 4)
